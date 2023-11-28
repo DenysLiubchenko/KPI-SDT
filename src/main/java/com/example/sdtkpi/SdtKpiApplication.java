b895da14a75b744864838a5e977e94ba1f7da09d
lubchenkodenys <denyslubchenko@gmail.com>
2023-11-28 11:17:27 +0200

    Initial commit

---
+++ src/main/java/com/example/sdtkpi/SdtKpiApplication.java
@@ -0,0 +1,13 @@
+package com.example.sdtkpi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdtKpiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SdtKpiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sdtkpi/SdtKpiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sdtkpi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdtKpiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
